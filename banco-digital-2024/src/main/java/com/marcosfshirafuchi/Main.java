c889c4f077ff74bd87c6b60c3141212ef19b5471
Marcos Shirafuchi <marcosfshirafuchi@gmail.com>
2024-05-09 21:47:17 -0300

    Atualização do desafio de Projeto da DIO: Criando um Banco Digital com Java e Orientação a Objetos

--- banco-digital-2024/src/main/java/com/marcosfshirafuchi/Main.java
+++ banco-digital-2024/src/main/java/com/marcosfshirafuchi/Main.java
@@ -7,6 +7,17 @@ import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * <h1>Criando um Banco Digital com Java e Orientação a Objetos</h1>
+     * Desafio de projeto da DIO
+     * <p>
+     * <b>Note:</b> Desenvolvido na linguagem Java.
+     *
+     * @author  Marcos Ferreira Shirafuchi
+     * @version 1.0
+     * @since   09/05/2024
+     */
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Locale.setDefault(Locale.US);
